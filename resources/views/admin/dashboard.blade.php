309ae0fb296ae44928b39fe3f26bf65d37014571
manakala19 <rizqanfajar19@gmail.com>
2023-09-11 14:51:50 +0700

    commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="bg-gray-900">
@@ -214,7 +216,6 @@
             </div>
         </div>
         </section>
-
 
 
 
