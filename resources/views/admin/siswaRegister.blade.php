0f553954c5af5742ceaf06565c32df127e4f175b
Irham Rizqan <irhamrizkan@gmail.com>
2023-09-14 14:11:22 +0700

    statusnya sakit

--- resources/views/admin/siswaRegister.blade.php
+++ resources/views/admin/siswaRegister.blade.php
@@ -21,7 +21,8 @@
                 <!-- Col -->
                 <div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
                     <h3 class="pt-4 text-2xl text-center">Buat akun siswa prakerin!</h3>
-                    <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                    <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded" method="POST">
+                        @csrf
                         <div class="mb-4 md:flex md:justify-between">
                             <div class="mb-4 md:mr-2 md:mb-0">
                                 <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
@@ -29,6 +30,7 @@
                                 </label>
                                 <input
                                     class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                    name="namaLengkap"
                                     id="namaLengkap"
                                     type="text"
                                     placeholder="Nama Lengkap"
@@ -38,12 +40,11 @@
                                 <label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
                                     Jurusan
                                 </label>
-                                <input
-                                    class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                    id="jurusan"
-                                    type="text"
-                                    placeholder="Multimedia atau RPL"
-                                />
+                                <select name="jurusan" id="jurusan">
+                                    <option value="" disabled>Pilih Jurusan</option>
+                                    <option value="1">Rekayasa Perangkat Lunak</option>
+                                    <option value="2">Multimedia</option>
+                                </select>
                             </div>
                             <div class="md:ml-2">
                                 <label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
@@ -51,6 +52,7 @@
                                 </label>
                                 <input
                                     class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                    name="tempat"
                                     id="tempat"
                                     type="text"
                                     placeholder="Tempat Siswa Prakerin"
@@ -63,6 +65,7 @@
                             </label>
                             <input
                                 class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                name="email"
                                 id="email"
                                 type="email"
                                 placeholder="Email"
@@ -75,6 +78,7 @@
                                 </label>
                                 <input
                                     class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                    name="password"
                                     id="password"
                                     type="password"
                                     placeholder="******************"
@@ -84,12 +88,9 @@
 
                         </div>
                         <div class="mb-6 text-center">
-                            <button
-                                class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                                type="button"
-                            >
-                                Buat Akun
-                            </button>
+                            <input type="submit"
+                            class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                            value="Buat Akun">
                         </div>
                         <hr class="mb-6 border-t" />
                         <div class="text-center">
