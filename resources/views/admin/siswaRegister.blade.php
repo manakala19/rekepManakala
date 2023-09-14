fbcda501d65db76409590c98de14ff4be62cb3e3
manakala19 <rizqanfajar19@gmail.com>
2023-09-14 13:35:28 +0700

    p

--- resources/views/admin/siswaRegister.blade.php
+++ resources/views/admin/siswaRegister.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Input Data Siswa</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="font-medium bg-gray-400">
     <!-- Container -->
