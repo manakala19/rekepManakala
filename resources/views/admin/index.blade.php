7e22e86cd9b4af20451654204f828a62efdaa4a0
Irham Rizqan <irhamrizkan@gmail.com>
2023-09-20 14:48:24 +0700

    tambahin dong

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,9 +12,9 @@
 
 <body class="bg-gray-900">
 
-    <nav class="fixed top-0 left-0 bg-gray-700 w-full shadow">
+    <nav class="fixed top-0 left-0 bg-gray-800 w-full shadow mb-36">
         <div class="container m-auto flex justify-between items-center text-white">
-            <h1 class="pl-8 py-6 text-xl font-bold hover: ">Rekep-App</h1>
+            <h1 class="pl-8 py-4 text-xl font-bold hover: ">Rekep-App</h1>
             <ul class="hidden md:flex items-center pr-10 text-base font-semibold cursor-pointer">
                 <li class="hover:bg-gray-800 py-4 px-6 rounded-2xl">Profil</li>
                 <li class="hover:bg-gray-800 py-4 px-6 rounded-2xl">Projek</li>
@@ -25,59 +25,33 @@
                 <div class="w-5 h-1 bg-gray-700 mb-1"></div>
                 <div class="w-5 h-1 bg-gray-700 mb-1"></div>
                 <div class="w-5 h-1 bg-gray-700 mb-1"></div>
-                <div class="absolute top-0 -right-full h-screen w-8/12 bg-gray-700 border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
+                <div class="absolute top-0 -right-full h-screen w-8/12 bg-gray-800 border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
                     <ul class="flex flex-col items-center w-full text-base cursor-pointer pt-10">
-                        <li class="hover:bg-gray-800 py-4 px-6 w-full">Profil</li>
-                        <li class="hover:bg-gray-800 py-4 px-6 w-full">Projek</li>
-                        <li class="hover:bg-gray-800 py-4 px-6 w-full">Logout</li>
+                        <li class="hover:bg-blue-500 py-4 px-6 w-full">Profil</li>
+                        <li class="hover:bg-blue-500 py-4 px-6 w-full">Projek</li>
+                        <li class="hover:bg-blue-500 py-4 px-6 w-full">Logout</li>
                     </ul>
                 </div>
             </button>
         </div>
     </nav>
 
-    <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 ">
+    <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 mt-16">
         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
             <!-- start sinii  -->
 
-            <div class="relative w-full">
-                <input type="search" id="search" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-white  dark:border-white dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Cari siswa atau jurusan.." required>
-                <button type="submit" class="absolute top-0 right-0 h-full p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
-                <span class="sr-only">Search</span>
-                </button>
-            </div>        
+
             <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                        <a href="admin/registrasiSiswa">
-                        <button type="button" class="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-                            <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                            </svg>
-                            Tambah data siswa
-                        </button>
-                        </a>
+
                         <div class="flex items-center space-x-3 w-full md:w-auto">
-                            <div id="actionsDropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="actionsDropdownButton">
-                                    <li>
-                                        <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"> Edit </a>
-                                    </li>
-                                </ul>
-                                <div class="py-1">
-                                    <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"> Hapus Semua</a>
-                                </div>
-                            </div>
-                            <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-                                </svg>
-                                Filter
-                                <svg class="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                    <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                                </svg>
-                            </button>
+                            <a href="admin/registrasiSiswa">
+                                <button type="button" class="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                                    <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                        <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                                    </svg>
+                                    Tambah data siswa
+                                </button>
+                                </a>
                         </div>
                     </div>
                 </div>
