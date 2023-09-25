71ab1f54cbb118f2cc2f6edcecc7b85bface586a
manakala19 <rizqanfajar19@gmail.com>
2023-09-25 11:42:23 +0700

    + catatan blade.php

--- resources/views/laporan_kegiatan/edit.blade.php
+++ resources/views/laporan_kegiatan/edit.blade.php
@@ -4,33 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edit Laporan</title>
+    <title>Create Laporan</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <center>
-    <h1>Edit Data</h1>
-    <form action="{{route('laporan_kegiatan.update', $laporan_kegiatan->id)}}" method="POST" enctype="multipart/form-data">
+<body class="bg-gray-900">
+
+  <nav class="relative flex w-full flex-wrap items-center justify-between bg-gray-900 py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-gray-900 lg:mt-3">
+    <div class="flex w-full flex-wrap items-center justify-between px-3">
+        <a class="ml-2 text-3xl text-white dark:text-neutral-200" href="#">Rekep App</a>
+        <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
+            <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                href="/logout">Logout</a>
+        </div>
+    </div>
+</nav>
+
+    <form action="{{route('laporan-kegiatan.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
-        @method('PUT');
-    <table border="2">
-        <th>User_ID</th>
-        <th>Tanggal</th>
-        <th>Hari</th>
-        <th>Minggu Ke</th>
-        <th>Kegiatan Kerja</th>
-        {{-- <th>Lampiran</th> --}}
-        <th>Aksi</th>
-        <tr>
-            <td><input type="hidden" name="user_id" value="{{Auth::id()}}"></td>
-            <td><input type="date" name="tanggal" id="" value="{{$laporan_kegiatan->tanggal}}"></td>
-            <td><input type="text" name="hari" id="" minlength="4" maxlength="6" value="{{$laporan_kegiatan->hari}}"></td>
-            <td><input type="number" name="minggu_ke" id="" maxlength="2" value="{{$laporan_kegiatan->minggu_ke}}"></td>
-            <td><input type="text" name="kegiatan_kerja_harian" id="" value="{{$laporan_kegiatan->kegiatan_kerja_harian}}"></td>
-            {{-- <td><input type="file" name="lampiran" id="" accept="image/*"></td> --}}
-            <td><input type="submit" value="Submit"></td>
-        </tr>
-    </table>
-    </form>
-    </center>
+            <!-- section mulaii      -->
+    <section class="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-10 mb-20">
+        <h1 class="text-xl font-bold text-white capitalize dark:text-white">Edit Laporan</h1>
+        <form action="" method="POST">
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <div>
+                    <label class="text-white dark:text-gray-200" for="username">User ID</label>
+                    <input id="username" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring disabled">
+                </div>
+
+                <div>
+                    <label class="text-white dark:text-gray-200" for="passwordConfirmation">Minggu Ke-</label>
+                    <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label class="text-white dark:text-gray-200" for="passwordConfirmation">Tanggal Mulai</label>
+                    <input id="date" type="date" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                </div>
+
+                <div>
+                    <label class="text-white dark:text-gray-200" for="passwordConfirmation">Tanggal Selesai</label>
+                    <input id="date" type="date" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                </div>
+
+            </div>
+        </form>
+        <div>
+            <label class="text-white dark:text-gray-200" for="passwordConfirmation"> Uraian Kegiatan</label>
+            <textarea id="textarea" type="textarea" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
+        </div>
+
+        <div>
+            <label class="block text-sm font-medium text-white">
+            Lampiran Kegiatan
+          </label>
+          <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+            <div class="space-y-1 text-center">
+              <svg class="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+              <div class="flex text-sm text-gray-600">
+                <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                  <span class="">Upload file</span>
+                  <input id="file-upload" name="file-upload" type="file" class="sr-only">
+                </label>
+                <p class="pl-1 text-white">Lampiran Kegiatan</p>
+              </div>
+              <p class="text-xs text-white">
+                PNG, JPG, GIF sampai dengan 10MB
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-end mt-6">
+            <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-gray-600">Simpan</button>
+        </div>
+    </section>
 </body>
 </html>
